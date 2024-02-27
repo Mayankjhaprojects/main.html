67b8ca848be685a88ccff36c9232e47e3f29793f
Mayank Jha <mayankmitthu06@gmail.com>
2024-02-27 22:49:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// let y = document.getElementById("edubtn");
+// let x= document.getElementById("education")
+// y.addEventListener("click",func);
+// function func(){
+//     console.log("clicked");
+//    x.scrollIntoView({behaviour: "smooth"});
+// }
+{
+let a = document.getElementById("aboutbtn");
+let b= document.getElementById("aboutpage")
+a.addEventListener("click",func);
+function func(){
+    console.log("clicked");
+   b.scrollIntoView({behaviour: "smooth"});
+}
+}
+{
+    let c = document.getElementById("homebtn");
+    let d= document.getElementById("home")
+    c.addEventListener("click",func);
+    function func(){
+        console.log("clicked");
+       d.scrollIntoView({behaviour: "smooth"});
+    }   
+}
+{
+    let e = document.getElementById("skillsbtn");
+    let f= document.getElementById("skills")
+    e.addEventListener("click",func);
+    function func(){
+        console.log("clicked");
+       f.scrollIntoView({behaviour: "smooth"});
+    }   
+}
+{
+    let g = document.getElementById("aboutbtn2");
+    let h= document.getElementById("aboutpage")
+    g.addEventListener("click",func);
+    function func(){
+        console.log("clicked");
+       h.scrollIntoView({behaviour: "smooth"});
+    }   
+}
+{
+    let i = document.getElementById("contactsbtn");
+    let j= document.getElementById("footer")
+    i.addEventListener("click",func);
+    function func(){
+        console.log("clicked");
+       j.scrollIntoView({behaviour: "smooth"});
+    }   
+}
